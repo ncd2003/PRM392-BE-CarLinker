722c8fdac529abbd8ad22d6e4a50783dd3d3184e
Mai Tuong Huy <143727030+maituonghuy@users.noreply.github.com>
2025-11-14 08:24:52 +0700

    Create ChangePasswordRequestDto.cs

---
+++ BusinessObjects/Models/DTOs/Auth/ChangePasswordRequestDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Models.DTOs.Auth
+{
+    public class ChangePasswordRequestDto
+    {
+        [Required(ErrorMessage = "Email là bắt buộc")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Mật khẩu mới là bắt buộc")]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự")]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
